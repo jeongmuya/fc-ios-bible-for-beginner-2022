d0d69eef15d7b39c08e02c273ed415a884b7cd0e
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2025-08-23 03:00:33 +0900

    :sparkles: SwiftUI-Basic-텍스트,이미지,버튼

---
+++ Part7_SwiftUI/Ch1_Swift-Basic/Ch1_Swift-Basic/ImageView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageView.swift
+//  Ch1_Swift-Basic
+//
+//  Created by YangJeongMu on 8/23/25.
+//
+
+import SwiftUI
+
+struct ImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageView()
+}
---
+++ Part7_SwiftUI/Ch1_Swift-Basic/Ch1_Swift-Basic/TextView.swift
@@ -0,0 +1,18 @@
+//
+//  TextView.swift
+//  Ch1_Swift-Basic
+//
+//  Created by YangJeongMu on 8/23/25.
+//
+
+import SwiftUI
+
+struct TextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextView()
+}
