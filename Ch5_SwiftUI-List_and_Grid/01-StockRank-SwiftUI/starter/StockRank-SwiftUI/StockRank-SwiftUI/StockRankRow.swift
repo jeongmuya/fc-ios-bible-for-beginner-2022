4f0d43107e2be48bd96891edcfe70a6269695943
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2025-08-27 23:49:34 +0900

    Ch5_SwiftUI-List_and_Grid- 01-StockRank-SwiftUI

---
+++ Ch5_SwiftUI-List_and_Grid/01-StockRank-SwiftUI/starter/StockRank-SwiftUI/StockRank-SwiftUI/StockRankRow.swift
@@ -0,0 +1,54 @@
+//
+//  StockRankRow.swift
+//  StockRank-SwiftUI
+//
+//  Created by YangJeongMu on 8/27/25.
+//
+
+import SwiftUI
+
+struct StockRankRow: View {
+    
+    var stock: StockModel
+    
+    var body: some View {
+        HStack {
+            Text("\(stock.rank)")
+                .font(.system(size: 16, weight: .bold, design: .default))
+                .frame(width: 30)
+                .foregroundStyle(.blue)
+            Image("\(stock.imageName)")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            VStack(alignment: .leading, spacing: 5) {
+                Text("\(stock.name)")
+                    .font(.system(size: 12))
+                    .foregroundColor(.white)
+                HStack {
+                    Text("\(stock.price) 원")
+                        .font(.system(size: 12))
+                        .foregroundColor(.white)
+                    Text("\(stock.diff * 100, specifier: "%.2f") %")
+                        .font(.system(size: 12))
+                        .foregroundColor(stock.diff > 0 ? .red : .blue)
+                }
+            }
+            Spacer()
+            
+            Image(systemName: "heart.fill")
+                .resizable()
+                .renderingMode(.template)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+                .foregroundColor(.gray)
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(.gray)
+    }
+}
+
+#Preview {
+    StockRankRow(stock: StockModel.list[2])
+}
