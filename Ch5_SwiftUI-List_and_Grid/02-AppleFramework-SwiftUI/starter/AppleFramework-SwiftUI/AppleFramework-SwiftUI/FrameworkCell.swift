1ae420a7f6a3a66cb4f7d74e73c14364dfc2bc14
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2025-08-29 21:29:00 +0900

    AppleFramework-SwiftUI

---
+++ Ch5_SwiftUI-List_and_Grid/02-AppleFramework-SwiftUI/starter/AppleFramework-SwiftUI/AppleFramework-SwiftUI/FrameworkCell.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkCell.swift
+//  AppleFramework-SwiftUI
+//
+//  Created by YangJeongMu on 8/29/25.
+//
+
+import SwiftUI
+
+struct FrameworkCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkCell()
+}
