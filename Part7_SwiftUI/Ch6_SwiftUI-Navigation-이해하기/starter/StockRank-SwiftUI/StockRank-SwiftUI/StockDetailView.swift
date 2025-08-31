8c0e2d6e4ba7be2ff6d285dd98e3ac7ce234d6e4
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2025-08-31 23:07:44 +0900

    Part5_Network - Ch1_Network In iOS

---
+++ Part7_SwiftUI/Ch6_SwiftUI-Navigation-이해하기/starter/StockRank-SwiftUI/StockRank-SwiftUI/StockDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  StockDetailView.swift
+//  StockRank-SwiftUI
+//
+//  Created by YangJeongMu on 8/30/25.
+//
+
+import SwiftUI
+
+struct StockDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StockDetailView()
+}
