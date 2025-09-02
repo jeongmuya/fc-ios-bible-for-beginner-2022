a91b5692772e33a0f89284b0e1bc870bc7d4a7c1
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2025-09-02 22:12:45 +0900

    09. 프로젝트 깃헙 프로필가져오기 -4

--- Part5_Network/Ch2_Building an App/01_GithubUserProfile/starter/GithubUserProfile/GithubUserProfile/SearchViewController.swift
+++ Part5_Network/Ch2_Building an App/01_GithubUserProfile/starter/GithubUserProfile/GithubUserProfile/SearchViewController.swift
@@ -11,6 +11,8 @@ import Kingfisher
 
 class UserProfileViewController: UIViewController {
     
+    let network = NetworkService(configuration: .default)
+    
     @Published private(set) var user: UserProfile?
     var subscriptions = Set<AnyCancellable>()
     
@@ -84,41 +86,13 @@ extension UserProfileViewController: UISearchBarDelegate {
         
         guard let keyword = searchBar.text, !keyword.isEmpty else { return }
         "https://github.com/\(keyword)"
+
+        let resource = Resource<UserProfile>(base: "https://api.github.com/", path: "users/\(keyword)", params: [:], header: ["Content-Type": "application/json"])
         
-        let base = "https://api.github.com/"
-        let path = "users/\(keyword)"
-        let params: [String: String] = [:]
-        let header: [String: String] = ["Content-Type": "application/json"]
-        
-        var urlComponents = URLComponents(string: base + path)!
-        let queryItems = params.map{ (key: String, value: String) in
-            return URLQueryItem(name: key, value: value)
-        }
-        urlComponents.queryItems = queryItems
-        
-        var request = URLRequest(url: urlComponents.url!)
-        header.forEach { (key: String, value: String) in
-            request.addValue(value, forHTTPHeaderField: key)
-        }
-        
-        URLSession.shared
-            .dataTaskPublisher(for: request)
-            .tryMap { result -> Data in
-                guard let response = result.response as? HTTPURLResponse,
-                      (200..<300).contains(response.statusCode) else {
-                    let response = result.response as? HTTPURLResponse
-                    let statusCode = response?.statusCode ?? -1
-                    throw NetworkError.responseError(statusCode: statusCode)
-                    
-                }
-                return result.data
-            }
-            .decode(type: UserProfile.self, decoder: JSONDecoder())
+        network.load(resource)
             .receive(on: RunLoop.main)
-            .sink { compltion in
-                print("comletion: \(compltion)")
-                
-                switch compltion {
+            .sink { completion in
+                switch completion {
                 case .failure(let error):
                     self.user = nil
                 case .finished: break
@@ -127,7 +101,5 @@ extension UserProfileViewController: UISearchBarDelegate {
                 self.user = user
             }.store(in: &subscriptions)
 
-                
             }
     }
-
