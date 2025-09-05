d20e85877fffac6e4b4497e4d4d219dfae38d9f0
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2025-09-05 22:29:36 +0900

    06. 프로젝트 _swift_environmentObject-06

--- Part7_SwiftUI/Ch7_SwiftUI-Data-다루기-2/02-EnvironmentObject/starter/EnvironmentObjTest/EnvironmentObjTest/ThirdView.swift
+++ Part7_SwiftUI/Ch7_SwiftUI-Data-다루기-2/02-EnvironmentObject/starter/EnvironmentObjTest/EnvironmentObjTest/ThirdView.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct ThirtdView: View {
+struct ThirdView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ThirtdView()
+    ThirdView()
 }
--- Part7_SwiftUI/Ch7_SwiftUI-Data-다루기-2/02-EnvironmentObject/starter/EnvironmentObjTest/EnvironmentObjTest/UserProfileSettings.swift
+++ Part7_SwiftUI/Ch7_SwiftUI-Data-다루기-2/02-EnvironmentObject/starter/EnvironmentObjTest/EnvironmentObjTest/UserProfileSettings.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+final class UserProfileSettings: ObservableObject {
+    @Published var name: String = ""
+    @Published var age: Int = 0
+    
+    func haveBirthDayParty() {
+        age += 1
+    }
+}
