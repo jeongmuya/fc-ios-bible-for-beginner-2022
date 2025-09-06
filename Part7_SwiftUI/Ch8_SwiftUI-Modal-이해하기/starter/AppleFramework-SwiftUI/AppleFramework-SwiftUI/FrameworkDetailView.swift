14c2e1e44ccced4dd3e327c26993f181b7671ac6
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2025-09-06 23:19:47 +0900

    01.프로젝트_swiftui_model_이해하기_01

---
+++ Part7_SwiftUI/Ch8_SwiftUI-Modal-이해하기/starter/AppleFramework-SwiftUI/AppleFramework-SwiftUI/FrameworkDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkDetailView.swift
+//  AppleFramework-SwiftUI
+//
+//  Created by YangJeongMu on 9/6/25.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkDetailView()
+}
---
+++ Part7_SwiftUI/Ch8_SwiftUI-Modal-이해하기/starter/AppleFramework-SwiftUI/AppleFramework-SwiftUI/FrameworkListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FrameworkListViewModel.swift
+//  AppleFramework-SwiftUI
+//
+//  Created by YangJeongMu on 9/6/25.
+//
+
+import Foundation
