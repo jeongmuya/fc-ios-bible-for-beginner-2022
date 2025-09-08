b23f40d808925a49586f856b948ca9ae818ba3c3
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2025-09-09 00:21:40 +0900

    8,19

---
+++ Part8_Projects/01-Toss_Benefits/starter/TossBenefitTab/TossBenefitTab/BenefitListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BenefitListViewController.swift
+//  TossBenefitTab
+//
+//  Created by YangJeongMu on 9/9/25.
+//
+
+import UIKit
+
+class BenefitListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "혜택"
+    }
+
+}
