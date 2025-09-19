3fe96582715faff8da0ef18e4b9c371d894405c4
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2025-09-19 23:17:34 +0900

    10. 프로젝트 토스 혜택 탭 따라해보기(10)

--- Part8_Projects/01-Toss_Benefits/starter/TossBenefitTab/TossBenefitTab/Home/BenefitListViewController.swift
+++ Part8_Projects/01-Toss_Benefits/starter/TossBenefitTab/TossBenefitTab/Home/BenefitListViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 class BenefitListViewController: UIViewController {
     
@@ -21,15 +22,25 @@ class BenefitListViewController: UIViewController {
     
     var datasource: UICollectionViewDiffableDataSource<Section, Item>!
     
-    var todaySectionItems: [AnyHashable] = TodaySectionItem(point: .default, today: .today).sectionItem
-    var otherSectionItems: [AnyHashable] = Benefit.others
+//    var todaySectionItems: [AnyHashable] = TodaySectionItem(point: .default, today: .today).sectionItem
+//    var otherSectionItems: [AnyHashable] = Benefit.others
+    
+    @Published var todaySectionItems: [AnyHashable] = []
+    @Published var otherSectionItems: [AnyHashable] = []
+    
+    var subscritpion = Set<AnyCancellable>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        
-        
+        configureCollectionView()
+        setupUI()
+        bind()
+    }
+    
+    private func setupUI() {
+        navigationItem.title = "혜택"
+    }
+    private func configureCollectionView() {
         datasource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
             
             guard let section = Section(rawValue: indexPath.section) else { return nil }
@@ -47,8 +58,39 @@ class BenefitListViewController: UIViewController {
         collectionView.collectionViewLayout = layout()
         collectionView.delegate = self
         
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
-        navigationItem.title = "혜택"
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.todaySectionItems = TodaySectionItem(point: .default, today: .today).sectionItem
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+            self.otherSectionItems = Benefit.others
+        }
+    }
+    
+    private func bind() {
+        $todaySectionItems
+            .receive(on: RunLoop.main)
+            .sink { items in
+                self.applySnapshot(items: items, section: .today)
+            }.store(in: &subscritpion)
+        
+        $otherSectionItems
+            .receive(on: RunLoop.main)
+            .sink { items in
+                self.applySnapshot(items: items, section: .other)
+            }.store(in: &subscritpion)
+    }
+    
+    private func applySnapshot(items: [Item], section: Section) {
+        var snapshot = datasource.snapshot()
+        snapshot.appendItems(items, toSection: section)
+        datasource.apply(snapshot)
     }
     
     private func configureCell(for section: Section, item: Item, collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell? {
