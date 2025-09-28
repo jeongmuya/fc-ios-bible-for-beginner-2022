ba1dba6c74ce6935cade291282406d958821426d
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2025-09-28 23:35:16 +0900

    07.프로젝트 당근 홈탭 리스트 따라하기(홈 구성하기) 07

---
+++ Part8_Projects/02-Carrot_Home/starter/CarrotHomeTab/CarrotHomeTab/Home/HomeViweModel.swift
@@ -0,0 +1,46 @@
+//
+//  HomeViweModel.swift
+//  CarrotHomeTab
+//
+//  Created by YangJeongMu on 9/28/25.
+//
+
+import Foundation
+import Combine
+
+
+final class HomeViweModel {
+    
+    let network: NetworkService
+    
+    @Published var items: [ItemInfo] = []
+    var subscriptions = Set<AnyCancellable>()
+    
+    let itemTapped = PassthroughSubject<ItemInfo, Never>()
+    
+    init(network: NetworkService) {
+        self.network = network
+    }
+    
+    func fetch() {
+        let resource: Resource<[ItemInfo]> = Resource(
+                base: "https://my-json-server.typicode.com/",
+                path: "cafielo/demo/products",
+                params: [:],
+                header: ["Content-Type": "application/json"]
+                  )
+        network.load(resource)
+            .receive(on: RunLoop.main)
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    print("--> error: \(error)")
+                case .finished:
+                    print("--> finished")
+                }
+            } receiveValue: { items  in
+                self.items = items
+            }.store(in: &subscriptions)
+    }
+    
+}
