279007fc1d35c717907e7d161eee324a069ac790
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2025-10-04 23:34:39 +0900

    12.프로젝트 당근 홈탭 리스트 따라하기(리스트 구현하기) 12

--- Part8_Projects/02-Carrot_Home/starter/CarrotHomeTab/CarrotHomeTab/Home/ItemInfoCell.swift
+++ Part8_Projects/02-Carrot_Home/starter/CarrotHomeTab/CarrotHomeTab/Home/ItemInfoCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ItemInfoCell: UICollectionViewCell {
     
@@ -16,12 +17,26 @@ class ItemInfoCell: UICollectionViewCell {
     @IBOutlet var numOfChatLabel: UILabel!
     @IBOutlet var numOfLikeLabel: UILabel!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        thumbnail.layer.cornerRadius = 10
+        thumbnail.layer.masksToBounds = true
+        thumbnail.tintColor = .systemGray
+    }
+    
     func configure(item: ItemInfo) {
         titleLabel.text = item.title
         descriptionLabel.text = item.title
         priceLabel.text = "\(formatNumber(item.price))원"
+        
         numOfChatLabel.text = "\(item.numOfChats)"
         numOfLikeLabel.text = "\(item.numOfLikes)"
+        
+        thumbnail.kf.setImage(
+            with: URL(string: item.thumbnailURL)!,
+            placeholder: UIImage(systemName: "hands.sparkles.fill")
+        )
+        
     }
     
     private func formatNumber(_ price: Int) -> String {
