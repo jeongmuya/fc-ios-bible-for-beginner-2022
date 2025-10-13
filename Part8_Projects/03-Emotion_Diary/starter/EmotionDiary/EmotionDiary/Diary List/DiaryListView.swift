d883cd8bf6bb6ebbd3dc16f2654751b2a1e55d98
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2025-10-13 23:42:50 +0900

    ch03-03.감정일기 만들기(섹션과 리스트)03

--- Part8_Projects/03-Emotion_Diary/starter/EmotionDiary/EmotionDiary/Diary List/DiaryListView.swift
+++ Part8_Projects/03-Emotion_Diary/starter/EmotionDiary/EmotionDiary/Diary List/DiaryListView.swift
@@ -9,8 +9,6 @@ import SwiftUI
 
 struct DiaryListView: View {
     
-    
-    
     @StateObject var vm: DiaryListViewModel
     
     let layout: [GridItem] = [
@@ -22,29 +20,74 @@ struct DiaryListView: View {
     ]
     
     var body: some View {
-        LazyVGrid(columns: layout) {
-            ForEach(vm.keys, id:\.self) { key in
-                Section {
-                    let items = vm.dic[key] ?? []
-                    let orderedItems = items.sorted(by: {
-                        $0.date < $1.date
-                    })
-                    ForEach(orderedItems) {
-                        item in
-                        MoodDiaryCell(diary: item)
-                            .frame(height:50)
+        NavigationView{
+            VStack {
+                
+                ScrollView {
+                    LazyVGrid(columns: layout) {
+                        ForEach(vm.keys, id:\.self) { key in
+                            Section {
+                                let items = vm.dic[key] ?? []
+                                let orderedItems = items.sorted(by: {
+                                    $0.date < $1.date
+                                })
+                                ForEach(orderedItems) {
+                                    item in
+                                    MoodDiaryCell(diary: item)
+                                        .frame(height:50)
+                                }
+                                
+                            } header: {
+                                Text(formattedSectionTitle(key))
+                                    .font(.system(size: 30, weight: .black, design: nil))
+                            }
+                            .frame(height: 60)
+                            .padding()
+                        }
                     }
-                    
-                } header: {
-                    Text(key)
+                }
+                HStack {
+                    Button {
+                        print("New Button Tapped")
+                    } label: {
+                        Image(systemName: "plus")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 20)
+                    }
+                    .frame(width: 80, height: 80)
+                    .foregroundStyle(.white)
+                    .background(Color.blue)
+                    .cornerRadius(40)
                 }
             }
-            
+            .navigationTitle("Emotion Diary")
         }
     }
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            DiaryListView(vm: DiaryListViewModel())
+}
+
+extension DiaryListView {
+    private func formattedSectionTitle(_ id: String) -> String {
+        let dateComponents = id
+            .components(separatedBy: "-")
+            .compactMap{ Int($0) }
+        guard let year = dateComponents.first, let month = dateComponents.last else {
+            return id
         }
+        
+        
+        let calendar = Calendar(identifier: .gregorian)
+        let dateComponent = DateComponents(calendar: calendar, year: year, month: month)
+        let date = dateComponent.date!
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMMM yyyy"
+        return formatter.string(from: date)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiaryListView(vm: DiaryListViewModel())
     }
 }
